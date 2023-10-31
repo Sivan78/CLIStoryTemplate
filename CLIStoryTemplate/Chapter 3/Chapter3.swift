b0e59347c5b26568736c52653a1c6bed5b57495f
Adryenne <dreamy26@icloud.com>
2023-10-31 10:11:24 -0400

    Adds chapter3 base story

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,5 +6,5 @@
 import Foundation
 
 func chapterThree() {
-   print("HelloThere")
+   print("Until one day Mitch's mother passed. Mitch’s father was greatly devastated by her demise that he barely left his room for days. He often reminisced on all the good times he’d shared with his late wife. He specifically missed her ability to make the best tea pots after a long day.")
 }
