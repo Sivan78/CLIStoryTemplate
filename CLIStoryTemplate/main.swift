71222c1febcae5c27b6c5cb0535180e7e537232b
Adryenne <dreamy26@icloud.com>
2023-11-01 11:54:38 -0400

    adds varibles and comments to chapter3

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -11,6 +11,11 @@ func readStory() {
     chapterThree()
     chapterFour()
     chapterFive()
+    // Where all the People of the story lives. 
+    let character1 = "Mitch"
+    let character2 = "Father"
+    let character3 = "Mother"
+    let teapot = "Chai"
 }
 
 readStory()
